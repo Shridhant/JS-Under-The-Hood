a6c5713bbe94026b8339b2c654d024d578ba6b7b
Shridhant <shridhantjaiswal@gmail.com>
2025-02-21 21:36:16 +0530

    just practicing after tiring day of travelling

---
+++ closure.js
@@ -0,0 +1,12 @@
+let a = 12;
+
+const b = 13;
+
+function c() {
+  var e = 14;
+  function d() {
+    console.log(e);
+  }
+  return d;
+}
+console.log(c());
---
+++ iterations/1.js
@@ -0,0 +1,54 @@
+//Print all the elements in a given array
+
+// arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//   const el = arr[i];
+//   console.log(el);
+// }
+
+// Print numbers from 1 to 10
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// Print numbers from 10 to 1
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// Print even numbers from 1 to 10
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   } else {
+//     console.log("Odd");
+//   }
+// }
+
+// Print all numbers from 1 to 10
+// let a = 0;
+// for (let i = 1; i <= 100; i++) {
+//   a = a + i;
+//   console.log(a);
+// }
+
+// Print square of numbers from 1 to 5
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i * i);
+// }
+
+// Print all the elements in an array and also adding them into a new array
+
+// let arr = [3, 6, 9, 12, 15];
+
+// let ar = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]);
+//   ar.push(arr[i]);
+// }
+// console.log(ar);
