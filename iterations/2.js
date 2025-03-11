a61dd099b901b555b978ef468c711e7bf1c94320
Shridhant <shridhantjaiswal@gmail.com>
2025-03-11 22:00:59 +0530

    new array practises

---
+++ iterations/2.js
@@ -0,0 +1,22 @@
+// // console.log(a);
+// console.log(b);
+
+// var b = "not so good";
+
+// let a = "Food";
+
+// console.log(a);
+
+{
+  // Declare a variable:
+  let bestFood = "Fish and Chips";
+
+  // Declare another variable:
+  let myBestMeal = function () {
+    console.log(bestFood);
+    var bestFood = "Vegetable Fried Rice";
+  };
+
+  // Invoke myBestMeal function:
+  myBestMeal();
+}
