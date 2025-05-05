6d337a20fd61d58e3341f8e003852a7af1eb1088
Shridhant <shridhantjaiswal@gmail.com>
2025-05-06 00:01:06 +0530

    lol

--- test.js
+++ test.js
@@ -2,6 +2,9 @@ const arr = [4,52,22,33,49]
 
 for (let i=0; i<arr.length; i++){
 	if (arr[i]%2==0){
+		if(arr[i]==4){
+				console.log("This is 4")
+		}
 		console.log("This is even", arr[i])
 	}
 	else{
